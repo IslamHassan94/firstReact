cc3dea43eb5dcecd6a59eb99faec5a21caf94847
ISLAM <islamhasan@hotmail.com>
2020-07-20 02:32:04 +0200

    react

---
+++ src/contacts.js
@@ -0,0 +1,32 @@
+const contacts = [
+  {
+    name: "Beyonce",
+    imgURL:
+      "https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg",
+    phone: "+123 456 789",
+    email: "b@beyonce.com"
+  },
+  {
+    name: "Jack Bauer",
+    imgURL:
+      "https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg",
+    phone: "+987 654 321",
+    email: "jack@nowhere.com"
+  },
+  {
+    name: "Chuck Norris",
+    imgURL:
+      "https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png",
+    phone: "+918 372 574",
+    email: "gmail@chucknorris.com"
+  },
+  {
+    name: "Islam Hassan",
+    imgURL:
+      "https://scontent.fcai16-1.fna.fbcdn.net/v/t1.0-9/484135_273533219418715_369935824_n.jpg?_nc_cat=104&_nc_sid=de6eea&_nc_oc=AQmRB-TJFGOof0zgjquhCuQT0XOJAcWXQmnl2jSWcEoF_YBRUM0KwyzQMvNljyFtib0&_nc_ht=scontent.fcai16-1.fna&oh=aad562b65a97b126918e1124127c2d7e&oe=5F2E7FDC",
+    phone: "0123456789",
+    email: "gmail@islamhassan.com"
+  }
+];
+
+export default contacts;
